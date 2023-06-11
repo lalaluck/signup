236a6869c307b457565d60724758bb456c4c9a6b
Dara <dara.kwokmw@gmail.com>
2023-06-12 00:09:54 +0800

    done

--- main.js
+++ main.js
@@ -1,68 +1,71 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-//let parse = require('parse/node');
+ const express = require('express');
+ const bodyParser = require('body-parser');
+ const dotenv = require('dotenv').config();
+ const https = require('https');
+ const url = require('url');
+ //let parse = require('parse/node');
 
 
-//const role = Parse.Object.extend('role');
-const app = express();
+ //const role = Parse.Object.extend('role');
+ const app = express();
 
-app.use(express.static('./public'));
-app.use(bodyParser.urlencoded({extended: true}));
+ app.use(express.static('public'));
+ //在server中找到public file
+ app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/signup.html');
-})
+ app.get('/', (req, res) => {
+     res.sendFile(__dirname + '/signup.html');
+ })
 
-app.post('/', (req, res) => {
-    const firstName = req.body.fName;
-    const lastName = req.body.lName;
-    const email = req.body.email;
+ app.post('/', (req, res) => {
+     const firstName = req.body.fName;
+     const lastName = req.body.lName;
+     const email = req.body.email;
+     //對應html輸入資料
+     const data = {
+         'members': [
+             {
+                 email_address: email,
+                 status: 'subscribed',
+                 merge_fields: {
+                     FNAME: firstName,
+                     LNAME: lastName
+                 }                
+             }
+         ]
+     }
 
-    const data = {
-        members: [
-            {
-                email_address: email,
-                status: 'subscribed',
-                merge_fields: {
-                    FNAME: firstName,
-                    LNAME: lastName
-                }                
-            }
-        ]
-    }
+     const jsonData = JSON.stringify(data);
+     const url = 'https://us9.api.mailchimp.com/3.0/lists/91e6fdcc89';
+     const options = {
+         url: 'https://us9.api.mailchimp.com/3.0/lists/91e6fdcc89',
+         method: 'POST',
+         auth: process.env.AUTH
+     }
+     const request = https.request(url, options, (response) => {
 
-    const jsonData = JSON.stringify(data);
-    const url = process.env.url;
-    const AUTH = process.env.auth;
-    const options = {
-        method: 'POST',
-        auth: AUTH
-    }
-    const request = https.request(url, options, (response) => {
+         if (response.statusCode === 200){
+             res.sendFile(__dirname + '/success.html');
+         } else {
+             res.sendFile(__dirname + '/failure.html');
+         }
 
-        if (response.statusCode === 200){
-            res.sendFile(__dirname + '/success.html');
-        } else {
-            res.sendFile(__dirname + '/failure.html');
-        }
+         response.on('data', (data) => {
+             console.log(JSON.parse(data));
+         })
+     })
 
-        response.on('data', (data) => {
-            console.log(JSON.parse(data));
-        })
-    })
+     request.write(jsonData);
+     request.end();
+ })
 
-    request.write(jsonData);
-    request.end();
-})
+ app.post('/failure', (req, res) => {
+     res.redirect('/');
+ })
 
-app.post('/failure', (req, res) => {
-    res.redirect('/');
-})
+ const port = process.env.PORT || 3000;
 
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
+ app.listen(port, () => {
     console.log('Server is running on port ' + port + '.');
-})
-
+ })
 
